10db56fd21955104f7c7e26771edd6f2ab52505a
Lucas <lucasprado.cnpq@natura.net>
2025-03-26 21:23:51 -0300

    chore: finalize project implementation, preparing for deployment - without Docker setup

--- src/components/ui/button/load-more.tsx
+++ src/components/ui/button/load-more.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { LoadMoreButtonProps } from "@/interfaces/button";
 
 export default function LoadMoreButton({ isLoading, onClick }: LoadMoreButtonProps) {
@@ -8,9 +9,23 @@ export default function LoadMoreButton({ isLoading, onClick }: LoadMoreButtonPro
           <div className="loading-bar" style={{ width: isLoading ? "100%" : "50%" }}></div>
         </div>
 
-        <button onClick={onClick} className="btn-load" disabled={isLoading}>
+        <motion.button
+          onClick={onClick}
+          className="btn-load"
+          disabled={isLoading}
+          whileHover={{
+            scale: 1.03, // Aumento suave no tamanho
+            transition: {
+              repeat: Infinity,
+              repeatType: "reverse",
+              duration: 0.6,
+              ease: "easeInOut",
+            },
+          }}
+          style={{cursor: "pointer"}}
+        >
           {isLoading ? "Carregando..." : "Carregar mais"}
-        </button>
+        </motion.button>
       </div>
     </div>
   );
